a418867a23609def34cf4fda49546207997a637a
AngeloChiarella <chiarella.gaetano@gmail.com>
2023-01-17 11:28:53 -0300

    Interceptando requisicoes e Criando filtro de seguranca

---
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -0,0 +1,25 @@
+package med.voll.api.infra.security;
+
+import java.io.IOException;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter{
+
+	// sempre que houver uma requisicao dentro da api, será chamado esse metodo
+	
+	@Override
+	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+			throws ServletException, IOException {
+		filterChain.doFilter(request, response); //Sempre chamar esse metodo para prosseguir com a requisicao, se nao tiver ele nao devolve nada
+	
+	}
+
+}
