f3cca27cb2ccc9ff0f8859f00896cb3ca87cc86d
AngeloChiarella <chiarella.gaetano@gmail.com>
2023-01-17 11:38:18 -0300

    Recuperando o Token

--- src/main/java/med/voll/api/infra/security/SecurityFilter.java
+++ src/main/java/med/voll/api/infra/security/SecurityFilter.java
@@ -18,8 +18,23 @@ public class SecurityFilter extends OncePerRequestFilter{
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
 			throws ServletException, IOException {
+		
+		var tokenJWT = recuperarToken(request);
+		
+		System.out.println(tokenJWT);
+		
 		filterChain.doFilter(request, response); //Sempre chamar esse metodo para prosseguir com a requisicao, se nao tiver ele nao devolve nada
 	
+	}
+
+	private String recuperarToken(HttpServletRequest request) {
+		
+		var authorizationHeader = request.getHeader("Authorization");
+		
+		if(null == authorizationHeader)
+			throw new RuntimeException("Token JWT nao enviado no cabecalho Authorization");
+		
+		return authorizationHeader.replace("Bearer ", "");
 	}
 
 }
